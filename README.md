1aeea3eff0ff4e576d7017b0f5200f1ef4ee10f7
Bungandy <andy@jak.web.id>
2013-11-17 08:44:57 +0700

    first commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ fb-preview
 ==========
 
 Facebook Canvas Preview
+
+![alt tag](https://photos-1.dropbox.com/t/0/AABzveH31Ta4d6J4fxwVjd-hUUHYh8cRVp7b-619G5FCgg/12/444959/png/1024x768/3/1384657200/0/2/FBPreview.png)
---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<title>FB Preview</title>
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+	<div class="header"><div class="element"></div></div>
+
+	<div class="content">
+		<div class="container">
+			<iframe src="http://work.jakweb.co/map/jumblewords/_html/landing.php" height="1000px" frameborder="0" />
+		</div>
+	</div>
+
+	<div class="footer">
+		<hr/>
+		<ul class="menu">
+			<li>About</li>
+			<li>Create Ad</li>
+			<li>Create Page</li>
+			<li>Developers</li>
+			<li>Careers</li>
+			<li>Privacy</li>
+			<li>Cookies</li>
+			<li>Terms</li>
+			<li>Help</li>
+		</ul>
+		<div class="clearfix"></div>
+		<ul class="menu two">
+			<li><span style="color:grey">Facebook &copy; 2013 &bull;</span> English (US)</li>
+		</ul>
+	</div>
+	
+</body>
+</html>
